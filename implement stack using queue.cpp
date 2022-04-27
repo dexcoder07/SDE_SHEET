cab0ed4045ff8128ddb125bf934159d7fb04d513
Satwik <satwikraj2@gmail.com>
2022-04-27 15:04:51 +0530

    c3

--- implement stack using queue.cpp
+++ implement stack using queue.cpp
@@ -1,7 +1,37 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-class StackUsingQueue{
+//Stack using one queue
+class StackUsingOneQueue{
+
+    queue<int> Q1;
+
+public:
+    void push_element(int val){
+        Q1.push(val);
+
+        int size = Q1.size();
+
+        for(int i = 1 ; i <= size-1 ; i ++){
+            int x = Q1.front();
+            Q1.pop();
+            Q1.push(x);
+        }
+    }
+
+    void pop_element(){
+        Q1.pop();
+    }
+
+    int peek_element(){
+        int r_element = Q1.front();
+        return r_element;
+    }
+};
+
+
+//Stack Using two queue
+class StackUsingTwoQueue{
     
     queue<int> que1;
     queue<int> que2;
@@ -33,7 +63,8 @@ public:
 
 int main(){
 
-    StackUsingQueue stkq;
+//Stack using two queue.
+    StackUsingTwoQueue stkq;
     stkq.push_element(1);
     stkq.push_element(2);
     stkq.pop_element();
@@ -46,5 +77,39 @@ int main(){
     stkq.push_element(6);
     stkq.pop_element();
     cout << stkq.peek_element();
+
+//Stack using one queue.
+    StackUsingOneQueue stk;
+    stk.push_element(1);
+    stk.push_element(2);
+    stk.push_element(3);
+    
+    stk.pop_element();
+
+    stk.push_element(4);
+    
+    cout << stk.peek_element() << endl;
+
+    stk.push_element(5);
+
+    stk.pop_element();
+
+    stk.push_element(6);
+    stk.push_element(7);
+
+    cout << stk.peek_element() << endl;
+
+    stk.push_element(8);
+
+    stk.pop_element();
+
+    stk.push_element(19);
+
+    cout << stk.peek_element() << endl;
+
+    stk.push_element(13);
+    stk.pop_element();
+
+    cout << stk.peek_element();
     return 0;
 }
---
+++ temp.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+
+int main(){
+
+    queue<int> que;
+
+    que.push(1);
+    que.push(2);
+    que.push(3);
+    que.push(4);
+    que.push(5);
+    que.push(7);
+
+    cout << que.front();
+    return 0;
+}
